f9c8233039f36057c16a88d0bb76d5fc120d8ad4
Caio Lucius <caiolucius2010@hotmail.com>
2024-05-28 18:39:27 -0300

    chore: refactor the start executable

--- start.bat
+++ start.bat
@@ -1,11 +1,26 @@
 @echo off
 
+REM 
 python -m venv .venv
 
+REM 
 call .\.venv\Scripts\activate
 
+REM 
 pip install --upgrade pip
 
+REM 
 pip install -r requirements.txt
 
 echo Ambiente virtual criado e dependências instaladas.
+
+REM E
+python main.py
+
+REM
+deactivate
+
+REM 
+echo Ambiente virtual desativado.
+echo Pressione qualquer tecla para fechar...
+pause
